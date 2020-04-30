8e1d221005918d3edcf05697e749449701a3eed7
“yourufo <36325396@qq.com>
2020-04-30 09:30:43 +0800

    【新增】分类model

---
+++ src/main/java/com/neusoft/bookstore/cate/model/Cate.java
@@ -0,0 +1,26 @@
+package com.neusoft.bookstore.cate.model;
+
+import com.neusoft.bookstore.util.BaseModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author Liang
+ * @date 2020/4/30 9:25
+ */
+@Data
+public class Cate extends BaseModel {
+
+    @ApiModelProperty("分类名称")
+    private String cateName;
+
+    @ApiModelProperty("分类编码名称")
+    private String cateCode;
+
+    @ApiModelProperty("备注")
+    private String remark;
+
+    @ApiModelProperty("父级分类编码")
+    private String parentCateCode;
+
+}
